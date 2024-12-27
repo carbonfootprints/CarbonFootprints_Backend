6818fa4dc34f33e06e2b1247f38bc646ca57fec9
kavipriya2912002 <kavipriya2912002@gmail.com>
2024-12-27 11:05:58 +0530

    cors added

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.use(express.json());
 
 // CORS Middleware Configuration
 const corsOptions = {
-    origin: ["http://localhost:5173", "https://your-frontend-production-domain.com"], // Add your frontend URL(s) here
+    origin: ["http://localhost:5173", "https://carbon-footprint-front-end.vercel.app/"], // Add your frontend URL(s) here
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"], // Add other custom headers if needed
 };
@@ -27,7 +27,7 @@ app.get('/', (req, res) => {
     res.send("API is running");
 });
 
-app.use('/api/user', userRoutes);
+app.use('/api/user', userRoutes);s
 app.use('/api/direct', directRoutes);
 
 // Error Handling Middleware
