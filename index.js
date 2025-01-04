1e5904bfd7119ac3bafeed1bd12fddd5604e3568
kavipriya2912002 <kavipriya2912002@gmail.com>
2025-01-04 14:27:33 +0530

    corss

--- index.js
+++ index.js
@@ -10,12 +10,7 @@ ConnectDB();
 
 const PORT = process.env.PORT || 3000;
 const app = express();
-app.use(
-  cors({
-    origin: '*', // Allow only this origin
-    credentials: true, // Allow credentials (cookies, headers, etc.)
-  })
-);
+app.use(cors("*"));
 app.use(express.json());
 
 app.get("/", (req, res) => {
