38f5f5bbe8c3d1964077d81e743c10417e3d80e5
kavipriya2912002 <kavipriya2912002@gmail.com>
2025-01-04 12:35:26 +0530

    indirect added

--- index.js
+++ index.js
@@ -10,28 +10,22 @@ ConnectDB();
 
 const PORT = process.env.PORT || 3000;
 const app = express();
-// const corsOptions = {
-//     origin: function (origin, callback) {
-//       const allowedOrigins = ['http://localhost:5173', 'https://carbon-footprint-front-end.vercel.app'];
-//       if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
-//         callback(null, true); // Allow the request
-//       } else {
-//         callback(new Error('Not allowed by CORS')); // Reject the request
-//       }
-//     },
-//     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
-//     allowedHeaders: ['Content-Type', 'Authorization'], // Specify allowed headers
-//     credentials: true, // Allow credentials (cookies, authorization headers, etc.)
-//   };
+const corsOptions = {
+    origin: function (origin, callback) {
+      const allowedOrigins = ['http://localhost:5173', 'https://carbon-footprint-front-end.vercel.app'];
+      if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+        callback(null, true); // Allow the request
+      } else {
+        callback(new Error('Not allowed by CORS')); // Reject the request
+      }
+    },
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    allowedHeaders: ['Content-Type', 'Authorization'], // Specify allowed headers
+    credentials: true, // Allow credentials (cookies, authorization headers, etc.)
+  };
   
   // Enable CORS with options
-  app.use(
-    cors({
-      origin: "https://carbon-footprint-front-end.vercel.app", // Replace with your frontend's domain
-      methods: ["GET", "POST"],
-      allowedHeaders: ["Content-Type"],
-    })
-  );
+  app.use(cors(corsOptions));
   
   // Handle preflight requests for all routes
   app.options('*', cors(corsOptions)); 
